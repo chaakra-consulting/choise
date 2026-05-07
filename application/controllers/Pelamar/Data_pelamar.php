52bafa543509aec91995704f435cdfa1cac477f6
Muhammad Adi Saputro <53963692+xyz970@users.noreply.github.com>
2026-05-07 09:14:49 +0700

    Update Data_pelamar.php

--- application/controllers/Pelamar/Data_pelamar.php
+++ application/controllers/Pelamar/Data_pelamar.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-error_reporting(0);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 require_once APPPATH . "libraries/tcpdf/PDFMerger.php";
 
